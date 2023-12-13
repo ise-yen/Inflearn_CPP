4f8a0e3b538d01495a193726cff00f14a8111733
YeEun <ydkrkek@gmail.com>
2023-12-13 19:05:58 +0900

    [Update] no61 : 경로 추적 코드 추가

--- Inflearn_AlgoIntro_CPP/Inflearn_AlgoIntro_CPP/Chapter03/no61.cpp
+++ Inflearn_AlgoIntro_CPP/Inflearn_AlgoIntro_CPP/Chapter03/no61.cpp
@@ -4,17 +4,25 @@
 using namespace std;
 
 vector<int> v;
+int path[11];
 int ans{};
 void sumSubset(int x, int N, int sum, int M) {
 	if (x == N) {
 		if (sum == M) {
 			ans++;
+			for (int i = 0; i < N; i++) {
+				cout << path[i] << " ";
+			}
+			cout << "\n";
 		}
 		return;
 	}
 
+	path[x] = v[x];
 	sumSubset(x + 1, N, sum + v[x], M);
+	path[x] = 0;
 	sumSubset(x + 1, N, sum, M);
+	path[x] = -v[x];
 	sumSubset(x + 1, N, sum - v[x], M);
 }
 
