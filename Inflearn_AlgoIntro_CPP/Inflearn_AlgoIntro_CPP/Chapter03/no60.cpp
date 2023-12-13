d977665f93b5bf0b2eeecfbe8b83a0a30219c234
YeEun <ydkrkek@gmail.com>
2023-12-13 18:41:42 +0900

    [Update] no60 : 정답 코드 추가

--- Inflearn_AlgoIntro_CPP/Inflearn_AlgoIntro_CPP/Chapter03/no60.cpp
+++ Inflearn_AlgoIntro_CPP/Inflearn_AlgoIntro_CPP/Chapter03/no60.cpp
@@ -6,6 +6,20 @@ using namespace std;
 vector<int> v;
 vector<bool> isVisit;
 bool isSame = false;
+
+void answer(int L, int sum, int N, int total) {
+	if (isSame) return;
+	if (L == N) {
+		if (sum == total - sum) {
+			isSame = true;
+		}
+	}
+	else {
+		answer(L + 1, sum + v[L], N, total);
+		answer(L + 1, sum, N, total);
+	}
+}
+
 void subset(int x, int N) {
 	if (x >= N) {
 		int sum1{}, sum2{};
@@ -30,7 +44,7 @@ void subset(int x, int N) {
 }
 
 int main() {
-	int N{};
+	int N{}, total{};
 	cin >> N;
 	isVisit.resize(N, false);
 
@@ -38,9 +52,11 @@ int main() {
 		int tmp{};
 		cin >> tmp;
 		v.push_back(tmp);
+		total += tmp;
 	}
 
-	subset(0, N);
+	//subset(0, N);
+	answer(0, 0, N, total);
 
 	if (isSame) cout << "YES";
 	else cout << "NO";
