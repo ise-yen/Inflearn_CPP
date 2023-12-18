37b15f726748dda805ebae78f9c67dbeac53c330
YeEun <ydkrkek@gmail.com>
2023-12-18 17:35:57 +0900

    [Update] no20 강의 정답 코드

--- Inflearn_AlgoIntro_CPP/Inflearn_AlgoIntro_CPP/Chapter01/no20.cpp
+++ Inflearn_AlgoIntro_CPP/Inflearn_AlgoIntro_CPP/Chapter01/no20.cpp
@@ -3,18 +3,29 @@
 
 using namespace std;
 
-int main() {
-	// 1 : 가위
-	// 2 : 바위
-	// 3 : 보
-	// 1 < 2
-	// 2 < 3
-	// 3 < 1
+vector<int> va, vb;
 
+// 1 : 가위
+// 2 : 바위
+// 3 : 보
+// 1 < 2
+// 2 < 3
+// 3 < 1
+
+void answer(int N) {
+	// A가 이기는 경우만 우선적으로 생각하기
+	for (int i = 0; i < N; i++) {
+		if (va[i] == vb[i]) cout << "D\n";
+		else if (va[i] == 1 && vb[i] == 3) cout << "A\n";
+		else if (va[i] == 2 && vb[i] == 1) cout << "A\n";
+		else if (va[i] == 3 && vb[i] == 2) cout << "A\n";
+		else cout << "B\n";
+	}
+}
+
+int main() {
 	int N{}, A{}, B{};
 	cin >> N;
-	vector<int> va;
-	vector<int> vb;
 	for (int i = 0; i < N; i++) {
 		cin >> A;
 		va.push_back(A);
@@ -31,5 +42,6 @@ int main() {
 		else if (va[i] < vb[i]) cout << "B\n";
 		else if (va[i] > vb[i]) cout << "A\n";
 	}
+
 	return 0;
 }
