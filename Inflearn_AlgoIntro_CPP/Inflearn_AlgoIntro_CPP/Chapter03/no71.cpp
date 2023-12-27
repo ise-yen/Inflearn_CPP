8acc9909ed1ea64b7f01020f17978f7952a70fa1
YeEun <ydkrkek@gmail.com>
2023-12-27 22:40:52 +0900

    [S4] no71 / 강의 해설 코드

--- Inflearn_AlgoIntro_CPP/Inflearn_AlgoIntro_CPP/Chapter03/no71.cpp
+++ Inflearn_AlgoIntro_CPP/Inflearn_AlgoIntro_CPP/Chapter03/no71.cpp
@@ -4,6 +4,31 @@
 using namespace std;
 
 int S{}, E{};
+int ch[10001]{};
+void answer(int front) {
+	int d[3] = { 1, -1, 5 };
+	queue<int> q;
+	q.push(front);
+	ch[S] = 1;
+	int pos{};
+	while (!q.empty()) {
+		front = q.front();
+		q.pop();
+		for (int i = 0; i < 3; i++) {
+			pos = front + d[i];
+			if (pos == E) {
+				cout << ch[front];
+				return;
+			}
+			if (ch[pos] == 0) {
+				ch[pos] = ch[front] + 1;
+				q.push(pos);
+			}
+		}
+	}
+
+	return;
+}
 
 void bfs(int now) {
 	queue<int> q;
