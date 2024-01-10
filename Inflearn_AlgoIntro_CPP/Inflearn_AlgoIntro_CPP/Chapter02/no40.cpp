789f7d1758d50e36d87ab1b8992714b89bd0840d
YeEun <ydkrkek@gmail.com>
2024-01-10 12:34:54 +0900

    [S2] no40 : 정답코드 추가

--- Inflearn_AlgoIntro_CPP/Inflearn_AlgoIntro_CPP/Chapter02/no40.cpp
+++ Inflearn_AlgoIntro_CPP/Inflearn_AlgoIntro_CPP/Chapter02/no40.cpp
@@ -5,11 +5,48 @@
 
 using namespace std;
 
-int main() {
+void Answer() {
+	int N{}, M{};
+	cin >> N;
+	vector<int> A;
+	vector<int> B;
+	vector<int> C;
+
+	for (int i = 0; i < N; i++) {
+		int tmp{};
+		cin >> tmp;
+		A.push_back(tmp);
+	}
+	sort(A.begin(), A.end());
+
+	cin >> M;
+	for (int i = 0; i < M; i++) {
+		int tmp{};
+		cin >> tmp;
+		B.push_back(tmp);
+	}
+	sort(B.begin(), B.end());
+
+	int pa{}, pb{};
+	while (pa < N && pb < M) {
+		if (A[pa] == B[pb]) {
+			C.push_back(A[pa++]);
+			pb++;
+		}
+		else if (A[pa] < B[pb]) pa++;
+		else pb++;
+	}
+
+	for (auto it : C) {
+		cout << it << " ";
+	}
+}
+
+void MyAnswer() {
 	int N{}, M{};
 	cin >> N;
 	unordered_set<int> A;
-	vector<int> answer;
+	vector<int> v;
 
 	for (int i = 0; i < N; i++) {
 		int tmp{};
@@ -23,15 +60,18 @@ int main() {
 		cin >> tmp;
 		auto it = A.find(tmp);
 		if (it != A.end()) {
-			answer.push_back(tmp);
+			v.push_back(tmp);
 		}
 	}
 
-	sort(answer.begin(), answer.end());
+	sort(v.begin(), v.end());
 
-	for (auto it : answer) {
+	for (auto it : v) {
 		cout << it << " ";
 	}
+}
 
+int main() {
+	MyAnswer();
 	return 0;
 }
