f4dd0e613695c17db2cd8300f8430bf20feac9d4
YeEun <ydkrkek@gmail.com>
2024-02-13 01:35:50 +0900

    [S2] no42 : 정답코드 이분검색으로 찾기. sort를 미리하고 진행

--- Inflearn_AlgoIntro_CPP/Inflearn_AlgoIntro_CPP/Chapter02/no42.cpp
+++ Inflearn_AlgoIntro_CPP/Inflearn_AlgoIntro_CPP/Chapter02/no42.cpp
@@ -4,16 +4,38 @@
 
 using namespace std;
 
+vector<int> v;
+int N{}, M{}, answer{};
+void binarySearch(int left, int right) {
+	if (left > right) return;
+
+	int mid = (left + right) / 2;
+	if (v[mid] == M) {
+		answer = mid + 1;
+		return;
+	}
+	else if (v[mid] > M) {
+		binarySearch(left, mid - 1);
+	}
+	else {
+		binarySearch(mid + 1, right);
+	}
+}
+
 int main() {
-	int N{}, M{}, answer{};
 	cin >> N >> M;
 
 	for (int i = 0; i < N; i++) {
 		int tmp{};
 		cin >> tmp;
-		if (tmp <= M) answer++;
+		if (tmp <= M) answer++; // 구현
+		v.push_back(tmp);
 	}
 
-	cout << answer;
+	cout << answer << "\n"; // 구현
+
+	sort(v.begin(), v.end());
+	binarySearch(0, N - 1);
+	cout << answer; // 이분검색
 	return 0;
 }
