31829823f0a2b3eaee91b048ba780b7da362e180
YeEun <ydkrkek@gmail.com>
2024-03-21 21:17:41 +0900

    Update) [S2] no55 : 정답코드 추가

--- Inflearn_AlgoIntro_CPP/Inflearn_AlgoIntro_CPP/Chapter02/no55.cpp
+++ Inflearn_AlgoIntro_CPP/Inflearn_AlgoIntro_CPP/Chapter02/no55.cpp
@@ -1,14 +1,14 @@
 #include<iostream>
 #include<stack>
 #include<string>
+#include<vector>
 
 using namespace std;
+int N{};
+stack<int> s;
+string answer{};
 
-int main() {
-	int N{};
-	cin >> N;
-	stack<int> s;
-	string answer{};
+void myCode(){
 	int num = 1;
 	for (int i = 0; i < N; i++) {
 		int tmp{};
@@ -26,7 +26,28 @@ int main() {
 	}
 
 	if(!s.empty()) answer = "impossible";
+}
 
+void answerCode() {
+	int pos = 1;
+	for (int i = 1; i <= N; i++) {
+		int tmp;
+		cin >> tmp;
+		s.push(tmp);
+		answer.push_back('P');
+		while (!s.empty() && s.top() == pos) {
+			s.pop();
+			answer.push_back('O');
+			pos++;
+		}
+	}
+
+	if (!s.empty()) answer = "impossible";
+}
+
+
+int main() {
+	cin >> N;
+	answerCode();
 	cout << answer;
-	return 0;
 }
