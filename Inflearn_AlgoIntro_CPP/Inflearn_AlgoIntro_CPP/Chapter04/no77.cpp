e41b4bfa3fcdb68aec681eefb7d40244d87f715e
YeEun <ydkrkek@gmail.com>
2024-03-22 17:58:53 +0900

    Update) [S4] no77 : 정답코드 - Union&Find

--- Inflearn_AlgoIntro_CPP/Inflearn_AlgoIntro_CPP/Chapter04/no77.cpp
+++ Inflearn_AlgoIntro_CPP/Inflearn_AlgoIntro_CPP/Chapter04/no77.cpp
@@ -12,7 +12,6 @@ int t1{}, t2{};
 string answer;
 
 vector<int> friends[MAX];
-
 void myCode_Linked() {
 	for (int i = 0; i < M; i++) {
 		int a{}, b{};
@@ -51,10 +50,47 @@ void myCode_Linked() {
 	else answer = "YES";
 }
 
+// 공통원소가 있으면 같은 집합
+// 없으면 서로소 집합
+// 집합 관계는 트리로 표현
+int unf[MAX]; // 집합번호. union & find
+int Find(int v) { // 집합번호 찾기
+
+	if (v == unf[v]) return v; // 자기자신이 집합번호라면 자기자신을 리턴
+	else { // 그게 아니면 집합번호 재귀 탐색
+		unf[v] = Find(unf[v]); // 메모이제이션
+		return unf[v];
+	}
+}
+
+int Union(int a, int b) { // 같은 집합으로 묶기
+	// 집합번호 탐색
+	a = Find(a);
+	b = Find(b);
+
+	// 집합번호가 서로 다르다면 같은 집합에 묶기
+	if (a != b) unf[a] = b;
+}
+
+void answerCode() {
+	// 집합번호를 가지자신으로 초기화
+	// unf[학생번호] = 집합번호
+	for (int i = 1; i <= N; i++) unf[i] = i;
+
+	// 친구 입력
+	for (int i = 0; i < M; i++) {
+		int a{}, b{};
+		cin >> a >> b;
+		Union(a, b); // 친구끼리는 같은 집합으로 묶기
+	}
+
+	cin >> t1 >> t2;
+	if (unf[t1] == unf[t2]) answer = "YES";
+	else answer = "NO";
+}
+
 int main() {
 	cin >> N >> M;
-
-	myCode_Linked();
 
 	cout << answer;
 
